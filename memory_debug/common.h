13a82ddb58056889addb937a695b9f3e6bd6be4f
yukkun007 <kato.yutaka@gmail.com>
2018-01-02 07:34:23 +0900

    新規作成

---
+++ memory_debug/common.h
@@ -0,0 +1,8 @@
+﻿/* Copyright 2017 yukkun007 */
+#pragma once
+
+// メモリリークを検出するため
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#define new  ::new( _NORMAL_BLOCK, __FILE__, __LINE__ )
+#endif	// _DEBUG
---
+++ memory_debug/main.cpp
@@ -0,0 +1,16 @@
+﻿/* Copyright 2017 yukkun007 */
+#include <crtdbg.h>
+#include <tchar.h>
+
+#include "memory_debug/common.h"
+
+int _tmain(int argc, _TCHAR* argv[]) {
+    // メモリリーク検出
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    int* leak_ptr = new int[2];
+
+    // leak!!!!!!!!!
+
+    return 0;
+}
